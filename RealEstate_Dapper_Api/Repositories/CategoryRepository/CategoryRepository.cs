43dd021a978af5346d25be11f19de2d0ef854211
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2023-11-13 14:07:06 +0300

    Kategorilerin Api ile Silinmesi ve Güncellenmesi

--- RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
@@ -25,6 +25,16 @@ namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
             }
         }
 
+        public async void DeleteCategory(int id)
+        {
+            string query = "Delete From Category Where CategoryID = @categoryID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@categoryID", id);
+            using (var connection = _context.CreateConnection()) 
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
 
         public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
         {
@@ -33,6 +43,20 @@ namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
             {
                 var values = await connection.QueryAsync<ResultCategoryDto>(query);
                 return values.ToList();
+            }
+        }
+
+        public async void UpdateCategory(UpdateCategoryDto categoryDto)
+        {
+            string query = "Update Category Set CategoryName = @categoryName, CategoryStatus = @categoryStatus Where CategoryID = @categoryID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@categoryName", categoryDto.CategoryName);
+            parameters.Add("@categoryStatus", categoryDto.CategoryStatus);
+            parameters.Add("@categoryID", categoryDto.CategoryID);
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
             }
         }
     }
